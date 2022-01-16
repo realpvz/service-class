1a33b09b64cde4c4ecf15887fadf679f5c91d782
Iman <i.parvizi@adam-barfi.ir>
2022-01-16 21:14:15 +0330

    publish stubs and introduce command

--- src/ServiceClassProvider.php
+++ src/ServiceClassProvider.php
@@ -2,13 +2,22 @@
 
 namespace Realpvz\ServiceClass;
 
+use App\Console\Commands\MakeServiceCommand;
 use Illuminate\Support\ServiceProvider;
 
 class ServiceClassProvider extends ServiceProvider
 {
+    private static array $commandNames = [
+        MakeServiceCommand::class,
+    ];
+
     public function boot()
     {
+        $this->publishes([
+            __DIR__ . '/Stubs' => resource_path('/stubs'),
+        ], 'stubs');
 
+        $this->commands(self::$commandNames);
     }
 
     public function register()
