a83b5d67428d21b563d0474cfe94d4b10a4f4001
Iman <i.parvizi@adam-barfi.ir>
2022-01-17 22:46:29 +0330

    fix path

--- src/Commands/MakeServiceCommand.php
+++ src/Commands/MakeServiceCommand.php
@@ -66,7 +66,7 @@ if (!function_exists('{{functionName}}')) {
      */
     protected function getStub(): string
     {
-        return $this->resolveStubPath('../Stubs/service.stub');
+        return $this->resolveStubPath('/../Stubs/service.stub');
     }
 
     /**
